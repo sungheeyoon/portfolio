1a1152fd72c38def3ab5dee3672f8f9ea1f43167
sungheeyoon <torushy@gmail.com>
2021-07-13 00:14:29 +0900

    Added Hover

--- src/Components/projects.js
+++ src/Components/projects.js
@@ -1,59 +1,83 @@
+import styled from "styled-components";
+
+const LProject = styled.div`
+  height: 512px;
+  width: 100wh;
+  margin: 0;
+  padding: 0;
+  display: flex;
+  justify-content: flex-end;
+
+  &:hover {
+    background-color: black;
+    opacity: 0.9;
+  }
+  transition: all 1s;
+`;
+const RProject = styled.div`
+  height: 512px;
+  width: 100wh;
+  margin: 0;
+  padding: 0;
+  display: flex;
+  justify-content: flex-start;
+  &:hover {
+    background-color: black;
+    opacity: 0.9;
+  }
+  transition: all 1s;
+`;
+
+const ProjectTitle = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: start;
+  justify-content: center;
+  margin: 0 5%;
+`;
+
 export const LeftTitleProject = ({ gitSrc, title, subtitle, imgSrc }) => {
   return (
-    <div
-      style={{
-        height: "512px",
-        display: "flex",
-        justifyContent: "flex-end",
-        marginBottom: "28px",
-        marginLeft: "5%",
-      }}
-    >
-      <a href={gitSrc} target="_blank" rel="noreferrer">
-        <div style={{ display: "flex" }}>
-          <div
-            style={{
-              display: "flex",
-              flexDirection: "column",
-              alignItems: "start",
-              justifyContent: "center",
-            }}
-          >
+    <a href={gitSrc} target="_blank" rel="noreferrer">
+      <LProject>
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "space-between",
+            alignItems: "center",
+            width: "100%",
+          }}
+        >
+          <ProjectTitle>
             <h2 style={{ color: "white" }}>{title}</h2>
             <h3>{subtitle}</h3>
-          </div>
-          <img src={imgSrc} alt="" />
+          </ProjectTitle>
+
+          <img style={{ zIndex: "-1", height: "512px" }} src={imgSrc} alt="" />
         </div>
-      </a>
-    </div>
+      </LProject>
+    </a>
   );
 };
 export const RightTitleProject = ({ gitSrc, title, subtitle, imgSrc }) => {
   return (
-    <div
-      style={{
-        height: "512px",
-        display: "flex",
-        justifyContent: "flex-start",
-        marginBottom: "28px",
-      }}
-    >
-      <a href={gitSrc} target="_blank" rel="noreferrer">
-        <div style={{ display: "flex" }}>
-          <img src={imgSrc} alt="" />
-          <div
-            style={{
-              display: "flex",
-              flexDirection: "column",
-              alignItems: "start",
-              justifyContent: "center",
-            }}
-          >
+    <a href={gitSrc} target="_blank" rel="noreferrer">
+      <RProject>
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "space-between",
+            alignItems: "center",
+            width: "100%",
+          }}
+        >
+          <img style={{ zIndex: "-1", height: "512px" }} src={imgSrc} alt="" />
+          <ProjectTitle>
             <h2 style={{ color: "white" }}>{title}</h2>
             <h3>{subtitle}</h3>
-          </div>
+          </ProjectTitle>
         </div>
-      </a>
-    </div>
+      </RProject>
+    </a>
   );
 };
